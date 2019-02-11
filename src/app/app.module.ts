023072fe38bfea83421235080508222cdabb03aa
sapla07 <s.sapla132@gmail.com>
2019-02-12 01:39:10 +0530

    Add header component and scss styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { headerComponent } from './common/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent
   ],
   imports: [
     BrowserModule
